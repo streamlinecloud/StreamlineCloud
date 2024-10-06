1d29b65a344cbe83b59c2047a12f97da59b06dcc
quinilo <hiquenvol@gmail.com>
2024-10-06 18:14:22 +0200

    Fixed post/server/start

--- streamlinecloud-main/src/main/java/io/streamlinemc/main/core/backend/RestController/post/StartServerController.java
+++ streamlinecloud-main/src/main/java/io/streamlinemc/main/core/backend/RestController/post/StartServerController.java
@@ -7,16 +7,17 @@ import io.streamlinemc.main.StreamlineCloud;
 import io.streamlinemc.main.core.server.CloudServer;
 import io.streamlinemc.main.utils.Cache;
 
+import java.util.Arrays;
+
 import static io.streamlinemc.main.core.backend.BackEndMain.mainPath;
 
 public class StartServerController {
 
     public StartServerController() {
-
         Cache.i().getBackend().post(mainPath + "post/server/start", ctx -> {
             StartServerPacket packet = new Gson().fromJson(ctx.body(), StartServerPacket.class);
 
-            //ctx.result(StreamlineCloud.startServerByGroup(StreamlineCloud.getGroupByName(packet.getGroup()), packet.getTemplates()));
+            ctx.result(StreamlineCloud.startServerByGroup(StreamlineCloud.getGroupByName(packet.getGroup()), Arrays.asList(packet.getTemplates())));
             ctx.status(200);
         });
 
