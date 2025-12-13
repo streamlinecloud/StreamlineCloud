29d146bade7d590d372b59224b72c3f89d20ed24
quinilo <hiquenvol@gmail.com>
2025-12-13 13:37:32 +0100

    Added WhitelistConfigurationPacket to manage whitelist settings

---
+++ streamlinecloud-api/src/main/java/net/streamlinecloud/api/packet/WhitelistConfigurationPacket.java
@@ -0,0 +1,26 @@
+package net.streamlinecloud.api.packet;
+
+import java.util.List;
+
+/**
+ * This class contains all information about the whitelist
+ */
+public class WhitelistConfigurationPacket {
+
+    /**
+     * Defines if the whitelist is enabled or disabled
+     */
+    boolean enabled;
+
+    /**
+     * A list of player names that can join while the whitelist is enabled
+     */
+    List<String> whitelistedPlayers;
+
+    /**
+     * A list of player names. All privileged players can use the ingame /server command.
+     * A join request from a privileged player can kick a non-privileged player if the network is full.
+     */
+    List<String> privilegedPlayers;
+
+}
