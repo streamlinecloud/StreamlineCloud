c4558d1eddde1dc19ccd7b9965d7f83f877d64c9
quinilo <hiquenvol@gmail.com>
2026-02-07 22:27:08 +0100

    Implemented penapi and swaggerui

--- streamlinecloud-backend/build.gradle.kts
+++ streamlinecloud-backend/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 }
 
 kotlin {
