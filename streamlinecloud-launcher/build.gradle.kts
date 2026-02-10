300fd4359d3c4212633b7ec113567666e9318041
quinilo <hiquenvol@gmail.com>
2026-02-10 17:00:36 +0100

    Launcher: gradle file cleanup

--- streamlinecloud-launcher/build.gradle.kts
+++ streamlinecloud-launcher/build.gradle.kts
@@ -1,8 +1,9 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("multiplatform")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 group = "net.streamlinecloud"
@@ -10,10 +11,11 @@ val version: String by rootProject
 val branch: String by rootProject
 
 kotlin {
-    jvm ()
-    linuxArm64()        // Linux
-    mingwX64()          // Windows
-    //macosArm64()      // macOS
+    jvm()
+    linuxX64()
+    linuxArm64()
+    mingwX64()
+    // macosArm64()
 
     linuxX64 {
         binaries {
@@ -40,11 +42,33 @@ kotlin {
     }
 
     sourceSets {
-        jvmMain.dependencies {
-            implementation("org.jetbrains.kotlin:kotlin-stdlib")
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+                implementation("com.squareup.okio:okio:3.16.4")
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib")
+            }
+        }
+
+        val nativeMain by creating {
+            dependsOn(commonMain)
+        }
+        val linuxX64Main by getting {
+            dependsOn(nativeMain)
+        }
+        val linuxArm64Main by getting {
+            dependsOn(nativeMain)
+        }
+        val mingwX64Main by getting {
+            dependsOn(nativeMain)
         }
     }
-
 }
 
 val nativeOutRoot: Directory = project.rootProject.layout.projectDirectory.dir("finished_builds/streamlinecloud-launcher")
@@ -54,23 +78,20 @@ tasks.register<Copy>("packageNativeLinuxX64") {
     from(layout.buildDirectory.file("bin/linuxX64/releaseExecutable/streamlinecloud-launcher.kexe"))
     into(nativeOutRoot)
     rename { _ -> "streamlinecloud-launcher-LINUX_X64" }
-    include("*")
 }
 
 tasks.register<Copy>("packageNativeLinuxArm64") {
     dependsOn("linkReleaseExecutableLinuxArm64")
-    from(layout.buildDirectory.dir("bin/linuxArm64/releaseExecutable/streamlinecloud-launcher.kexe"))
+    from(layout.buildDirectory.file("bin/linuxArm64/releaseExecutable/streamlinecloud-launcher.kexe"))
     into(nativeOutRoot)
     rename { _ -> "streamlinecloud-launcher-LINUX_ARM64" }
-    include("*")
 }
 
 tasks.register<Copy>("packageNativeMingwX64") {
     dependsOn("linkReleaseExecutableMingwX64")
-    from(layout.buildDirectory.dir("bin/mingwX64/releaseExecutable/streamlinecloud-launcher.exe"))
+    from(layout.buildDirectory.file("bin/mingwX64/releaseExecutable/streamlinecloud-launcher.exe"))
     into(nativeOutRoot)
     rename { _ -> "streamlinecloud-launcher-WIN_X64" }
-    include("*")
 }
 
 tasks.register("packageAllNative") {
@@ -93,12 +114,6 @@ tasks.register<ShadowJar>("shadowJar") {
         ?: project.configurations.getByName("runtimeClasspath")
 
     rt.filter { it.exists() }.forEach { f ->
-        if (f.isDirectory) {
-            from(f)
-        } else {
-            from(zipTree(f))
-        }
+        if (f.isDirectory) from(f) else from(zipTree(f))
     }
-
 }
-
