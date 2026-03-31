34e437c6de10232c007ab3adb6a89dc8f525f13f
quinilo <hiquenvol@gmail.com>
2026-03-31 21:10:34 +0200

    Backend: Updated GroupController to use PUT for create and removed unused import in SessionController

--- streamlinecloud-backend/src/main/kotlin/net/streamlinecloud/backend/controller/GroupController.kt
+++ streamlinecloud-backend/src/main/kotlin/net/streamlinecloud/backend/controller/GroupController.kt
@@ -4,7 +4,7 @@ import net.streamlinecloud.api.group.StreamlineGroup
 import net.streamlinecloud.backend.repository.GroupRepository
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -18,7 +18,7 @@ class GroupController (private val groupRepository: GroupRepository) {
     @ResponseStatus(HttpStatus.OK)
     fun list() = groupRepository.findAll().toList()
 
-    @PostMapping
+    @PutMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody group: StreamlineGroup): StreamlineGroup = groupRepository.save(group)
 
--- streamlinecloud-backend/src/main/kotlin/net/streamlinecloud/backend/controller/SessionController.kt
+++ streamlinecloud-backend/src/main/kotlin/net/streamlinecloud/backend/controller/SessionController.kt
@@ -4,7 +4,6 @@ import net.streamlinecloud.api.node.StreamlineNode
 import net.streamlinecloud.backend.repository.NodeRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
