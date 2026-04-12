7eeadd076d71918960784983a76b67c77f4b11a2
quinilo <hiquenvol@gmail.com>
2026-04-12 10:13:14 +0200

    Backend: Added Swagger configuration for authentification

---
+++ streamlinecloud-backend/src/main/kotlin/net/streamlinecloud/backend/config/SwaggerConfig.kt
@@ -0,0 +1,37 @@
+package net.streamlinecloud.backend.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("StreamlineCloud API")
+                    .version("1.0.0")
+            )
+            .addSecurityItem(
+                SecurityRequirement().addList("Bearer Authentication")
+            )
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "Bearer Authentication",
+                        SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("API Key")
+                            .description("Enter an API key")
+                    )
+            )
+    }
+}
