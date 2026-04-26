498b06511206fbee88794b0f8b58bb0d6cc96754
quinilo <hiquenvol@gmail.com>
2026-04-26 12:49:26 +0200

    Client: Javadoc and additional features for the new GroupManager.kt

--- streamlinecloud-client/src/main/kotlin/net/streamlinecloud/client/manager/GroupManager.kt
+++ streamlinecloud-client/src/main/kotlin/net/streamlinecloud/client/manager/GroupManager.kt
@@ -7,13 +7,19 @@ import net.streamlinecloud.api.socket.SocketResponse
 import net.streamlinecloud.client.adapter.SocketAdapter
 import net.streamlinecloud.client.core.StreamlineApiClient
 
+/**
+ * An api class for managing all [StreamlineGroup]'s existing in the connected cluster.
+ * Changes are updatet in real time due to the socket connection.
+ *
+ * **Important:** This is not the place where the data is saved. This class only mirrors the data and sends the changes to the backend.
+ */
 class GroupManager(
     val apiClient: StreamlineApiClient
 ) : SocketAdapter {
 
     override val topic: String = "/topic/groups"
 
-    val groups: MutableList<StreamlineGroup> = mutableListOf()
+    private val groups: MutableList<StreamlineGroup> = mutableListOf()
 
     suspend fun init() {
         Gson().fromJson(apiClient.httpClient.get("/groups").bodyAsText(), List::class.java).forEach { group ->
@@ -21,6 +27,9 @@ class GroupManager(
         }
     }
 
+    /**
+     * An internal function used to r receive new data
+     */
     override fun receive(response: SocketResponse) {
         val new: StreamlineGroup = Gson().fromJson(response.content.toString(), StreamlineGroup::class.java)
 
@@ -28,16 +37,48 @@ class GroupManager(
         groups.add(new)
     }
 
-    suspend fun create(group: StreamlineGroup) {
-        if (groups.stream().anyMatch { existingGroup -> existingGroup.name == group.name }) return
+    /**
+     * This functions saves and activates a new group.
+     * @param group A new CloudGroup instance
+     * @return True if created successfully, false otherwise
+     */
+    suspend fun create(group: StreamlineGroup): Boolean {
+        if (groupExists(group.name)) return false
         apiClient.httpClient.put("/groups", Gson().toJson(group))
+        return true
     }
 
-    suspend fun update(group: StreamlineGroup) {
+    /**
+     * Updates a group based on its name
+     *
+     * @param group The group with the old name and the updated data
+     * @return True if updated successfully, false otherwise
+     */
+    suspend fun update(group: StreamlineGroup): Boolean {
+        if (!groupExists(group.name)) return false
         apiClient.httpClient.put("/groups", Gson().toJson(group))
+        return true
     }
 
     fun delete(group: StreamlineGroup) {
         TODO("NOT YET IMPLEMENTED")
+    }
+
+    /**
+     * Finds a group by its name.
+     *
+     * @return The [StreamlineGroup] with the given name, or `null` if not found.
+     */
+    fun getGroup(name: String): StreamlineGroup? {
+        return groups.find { group -> group.name == name }
+    }
+
+    /**
+     * Checks if a group with a specific name exists
+     *
+     * @return True if a [StreamlineGroup] with the given name exists, otherwise false
+     */
+    fun groupExists(name: String): Boolean {
+        return groups.stream().anyMatch { existingGroup -> existingGroup.name == name }
     }
 }
