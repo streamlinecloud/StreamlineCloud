8d325450aa4797859087b6eab3f9c48bde4a3f75
quinilo <hiquenvol@gmail.com>
2026-05-04 11:14:33 +0200

    Backend: excluded `UserDetailsServiceAutoConfiguration` from ApplicationConf to prevent spring security from generating a test password

--- streamlinecloud-backend/src/main/kotlin/net/streamlinecloud/backend/StreamlineBackend.kt
+++ streamlinecloud-backend/src/main/kotlin/net/streamlinecloud/backend/StreamlineBackend.kt
@@ -3,6 +3,8 @@ package net.streamlinecloud.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.persistence.autoconfigure.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @EntityScan(
@@ -16,7 +18,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
         "net.streamlinecloud.backend.repository"
     ]
 )
-@SpringBootApplication
+@SpringBootApplication(exclude = [
+    UserDetailsServiceAutoConfiguration::class,
+])
 class StreamlinecloudBackendApplication
 
 fun main(args: Array<String>) {
