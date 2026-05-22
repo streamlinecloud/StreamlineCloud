673af1e3dab18f02d92360361a24073d6513b448
quinilo <hiquenvol@gmail.com>
2026-05-22 16:44:48 +0200

    Node: GroupsCommand: ported set flied subcommand

--- streamlinecloud-node/src/main/java/net/streamlinecloud/main/command/GroupsCommand.java
+++ streamlinecloud-node/src/main/java/net/streamlinecloud/main/command/GroupsCommand.java
@@ -14,13 +14,14 @@ import net.streamlinecloud.main.terminal.command.SubcommandCheckExecute;
 import net.streamlinecloud.main.utils.Utils;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class GroupsCommand extends StreamlineCommand {
 
+    static List<String> classFields = Arrays.stream(StreamlineGroup.class.getDeclaredFields()).toList().stream().map(Field::getName).toList();
+
     public GroupsCommand() {
+
         super("groups", "Manage all groups", new StreamlineCommandTree()
                 .add(new StreamlineSubcommand("list", ((variables, logger) -> {
                     logger.info("sc.command.groups.list.title");
@@ -72,7 +73,8 @@ public class GroupsCommand extends StreamlineCommand {
                     }
                     return data("group", group);
 
-                })).add(new StreamlineSubcommand("group %name", ((variables, logger) -> {
+                }))
+                .add(new StreamlineSubcommand("group %name", ((variables, logger) -> {
                     StreamlineGroup group = (StreamlineGroup) variables.get("group");
                     HashMap<String, Object> fields = new HashMap<>();
 
@@ -89,7 +91,8 @@ public class GroupsCommand extends StreamlineCommand {
 
                     StreamlineCloud.log("Information about " + group.getName());
                     fields.forEach((k, v) -> StreamlineCloud.log(k + ": " + v));
-                }))).add(new StreamlineSubcommand("group %name delete", ((variables, logger) -> {
+                })))
+                .add(new StreamlineSubcommand("group %name delete", ((variables, logger) -> {
 
                     StreamlineGroup group = StreamlineCloud.getGroupManager().getGroup(variables.get("name").toString());
 
@@ -103,6 +106,33 @@ public class GroupsCommand extends StreamlineCommand {
                         StreamlineCloud.log("sc.command.groups.notFound", new ReplacePaket[]{new ReplacePaket("%1", variables.get("name").toString())});
 
                     }
+
+                })))
+                .addCompleter("group %name set %field", () -> classFields)
+                .add(new StreamlineSubcommand("group %name set %field %value", ((variables, logger) -> {
+                    StreamlineGroup group = StreamlineCloud.getGroupManager().getGroup(variables.get("name").toString());
+                    String field = variables.get("field").toString();
+                    String value = variables.get("value").toString();
+
+                    if (field.equals("name") || field.equals("runtime") || field.equals("staticGroup")) {
+                        logger.warning("You cannot update this field while StreamlineCloud is running. We recommend deleting the group and creating a new one.");
+                        return;
+                    }
+
+                    try {
+                        assert group != null;
+                        Utils.setFieldsFromMap(group, Map.of(
+                                field, value
+                        ));
+                    } catch (IllegalAccessException | NoSuchFieldException e) {
+                        throw new RuntimeException(e);
+                    } catch (NumberFormatException e) {
+                        logger.warning("Please enter a valid number");
+                    }
+
+                    StreamlineCloud.getGroupManager().update(group);
+
+                    logger.info("Set " + field + " to " + value);
 
                 }))));
         setDefaultSubCommand("list");
@@ -147,43 +177,6 @@ public class GroupsCommand extends StreamlineCommand {
                         String groupSub = args[3];
 
                         switch (groupSub) {
-                            case "set" -> {
-
-                                String setSub = args[4];
-
-                                switch (setSub) {
-                                    case "templates":
-                                    case "name":
-                                        break;
-
-                                    default:
-
-                                        /*if (!fields.containsKey(setSub)) {
-                                            StreamlineCloud.log("Please enter a valid variable");
-                                            return;
-                                        }*/
-
-                                        if (args.length != 6) {
-                                            StreamlineCloud.log("Please enter a value");
-                                            return;
-                                        }
-
-                                        Map<String, Object> values = Map.of(
-                                                setSub, args[5]
-                                        );
-
-                                        try {
-                                            Utils.setFieldsFromMap(group, values);
-                                        } catch (IllegalAccessException | NoSuchFieldException e) {
-                                            throw new RuntimeException(e);
-                                        } catch (NumberFormatException e) {
-                                            StreamlineCloud.log("Please enter a valid number");
-                                        }
-                                        break;
-
-                                }
-
-                            }
                             case "add" -> {
 
                                 String addSub = args[4];
@@ -215,18 +208,6 @@ public class GroupsCommand extends StreamlineCommand {
                                     }
 
                                 }
-                            }
-
-                            case "priority" -> {
-                                String prioritySub = args[4];
-                                int priority = 0;
-                                try {
-                                    priority = Integer.parseInt(prioritySub);
-                                } catch (NumberFormatException e) {
-                                    StreamlineCloud.getLogger().error("sc.command.groups.priority.nan");
-                                }
-                                group.setPriority(priority);
-                                StreamlineCloud.log("sc.command.groups.priority.success");
                             }
                         }
 
