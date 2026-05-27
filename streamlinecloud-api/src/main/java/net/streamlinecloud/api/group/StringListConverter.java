b5d97d4ff2ae39edd90f8135d827959cdac9c5ab
quinilo <hiquenvol@gmail.com>
2026-05-27 10:46:02 +0200

    Node: Refactored `StringListConverter` to use raw `List` to handle freshly created groups.

--- streamlinecloud-api/src/main/java/net/streamlinecloud/api/group/StringListConverter.java
+++ streamlinecloud-api/src/main/java/net/streamlinecloud/api/group/StringListConverter.java
@@ -6,16 +6,16 @@ import jakarta.persistence.Converter;
 import java.util.List;
 
 @Converter
-public class StringListConverter implements AttributeConverter<List<String>, String> {
+public class StringListConverter implements AttributeConverter<List, String> {
     private static final Gson gson = new Gson();
 
     @Override
-    public String convertToDatabaseColumn(List<String> attribute) {
+    public String convertToDatabaseColumn(List attribute) {
         return (attribute == null) ? "[]" : gson.toJson(attribute);
     }
 
     @Override
-    public List<String> convertToEntityAttribute(String dbData) {
+    public List convertToEntityAttribute(String dbData) {
         if (dbData == null || dbData.isEmpty()) return List.of();
         return gson.fromJson(dbData, List.class);
     }
