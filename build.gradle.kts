1a68b76ca59cd051d3b166a854219fa2af525b9e
quinilo <hiquenvol@gmail.com>
2026-08-18 20:00:47 +0200

    Compiler: Forcing Java 21 for all sub modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,16 @@ plugins {
 
     id("org.springframework.boot") version "4.0.2" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
-    id("java")
     kotlin("plugin.jpa") version "2.3.10"
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+subprojects {
+    plugins.withId("java") {
+        extensions.configure<JavaPluginExtension> {
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of(21))
+            }
+        }
     }
 }
 
